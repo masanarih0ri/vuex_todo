d4284c9df72bc634006a393694c01a4422703603
masanarih0ri <masanarihoridevelop@gmail.com>
2021-04-05 23:02:40 +0900

    helloworldコンポーネントに変えてcounterコンポーネントを作成

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Counter from '@/components/Counter'
 
-describe('HelloWorld.vue', () => {
+describe('Counter.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(Counter)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
